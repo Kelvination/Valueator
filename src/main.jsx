13f9e2e19d2eeb0b82b6636277b0fa01052df611
Kelvin Newton <swimmingdrummer@gmail.com>
2024-12-03 21:39:17 +0000

    try again?

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,5 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename={"/Valueator/"}>
-        <App />
-    </BrowserRouter>
+    <App />
 )
